f1a6fcb679ac6b9bc924a58ddefb8d4aef1957ad
dragonrain5761 <dragonrain5761@gmail.com>
2023-07-14 21:57:47 +0900

    캘린더 이미지 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,20 +6,21 @@ import { useDispatch, useSelector } from "react-redux";
 import { toogleModal } from "../redux/modules/modal";
 
 const Home = () => {
-  const { modalChecked } = useSelector((state) => state.modal);
-  const dispatch = useDispatch();
-  const onClickModalHandler = () => {
-    dispatch(toogleModal());
+  // const { modalChecked } = useSelector((state) => state.modal);
+  // const dispatch = useDispatch();
+  // const onClickModalHandler = () => {
+  //   dispatch(toogleModal());
   return (
     <Div>
       <Calendar></Calendar>
       <List></List>
-      <Modal />
+      {/* <Modal />
       <Div>asdf</Div>
-      <button onClick={onClickModalHandler}>modal</button>
+      <button onClick={onClickModalHandler}>modal</button> */}
     </Div>
   );
 };
+// };
 
 export default Home;
 
