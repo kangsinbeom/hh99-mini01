87db4c47bfd151ac8844a4eec85e6f400ab7d13b
kangsinbeom <83047601+kangsinbeom@users.noreply.github.com>
2023-07-15 20:36:56 +0900

    css modal

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -8,8 +8,11 @@ export default Input;
 const WrapperInput = styled.input`
   outline: none;
   border: none;
-
+  border-bottom: 2px solid black;
+  background-color: transparent;
   transition: border-bottom-width 0.3s ease-in;
+  padding-left: 5%;
+
   &:focus {
     border-bottom: 2px solid black;
   }
