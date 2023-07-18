611eb6b390f4eb605174dc18fa40a905e09f112a
dragonrain5761 <dragonrain5761@gmail.com>
2023-07-18 22:06:21 +0900

    삭제, 수정기능 추가

--- src/apis/api.js
+++ src/apis/api.js
@@ -24,17 +24,18 @@ const deleteTodo = async (id) => {
   return response.data;
 };
 
+const updateTodo = async (id, updatedTodo) => {
+  const response = await instance.put(`/todos/${id}`, updatedTodo);
+  return response.data;
+};
 
 const getTodo = async (id) => {
-  const response = await instance.get("/todos")
-  const todo = response.data.filter(todo => todo.id === parseInt(id));
-  return todo
-} 
+  const response = await instance.get("/todos");
+  const todo = response.data.filter((todo) => todo.id === parseInt(id));
+  return todo;
+};
 
-
-export {getTodos, addTodo, getTodo}
-
+export { getTodos, addTodo, getTodo, deleteTodo, updateTodo };
 
 //  get으로 이렇게 받아오는 방식이랑 가져와서 캐쉬 된 데이터를 꺼내쓰는거랑
 // 뭐가 더 효율적인 일인가??
-
